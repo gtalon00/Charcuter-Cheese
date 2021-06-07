b347c06f6098e92036557ee5e1ea652057bff1ce
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-07 16:28:04 -0400

    basic form layout

--- src/components/UserOpinion.jsx
+++ src/components/UserOpinion.jsx
@@ -1,12 +1,34 @@
-import Form from "./inputs/Form"
-import React from 'react'
+import {useState} from 'react'
 
 export default function UserOpinion() {
+  const newComment = {
+    name: "",
+    input: "",
+  }
+  const [input, setInput] = useState()
   return (
     <div>
-        Give us your feedback! What cheeses
+      <br />
+      <label> Give us your feedback! What cheeses
         or pairings are you a fan of or
-        would you like to see
+        would you like to see?</label>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name/Anonymous"
+        />
+        <br />
+        <input 
+          style={{height: 100}}
+          type="text"
+          name="input"
+          placeholder="Your thoughts!"
+        />
+        <br />
+        <button >Submit</button>
+      </form>
+      
     </div>
   )
 }
