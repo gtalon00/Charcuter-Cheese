60f816a5dd3179cddedef1bb457c8b42dd3d7e8d
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-08 16:53:58 -0400

    more home view css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <br />
       <h1>Charcuter-Cheese</h1>
         <br />
-      <h2>Find your perfect charcuterie pairings</h2>
+      <h2>Find the perfect charcuterie pairings <br /> for you</h2>
         <br />
       <p>Look at our suggestions and other user's for your ideal board!</p>
       <Navbar />
