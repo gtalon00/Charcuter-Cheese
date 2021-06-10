976800944fcc2da3b85e818c1d70dc26be39e7fe
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-10 09:13:02 -0400

    css user opinion

--- src/components/UserOpinion.jsx
+++ src/components/UserOpinion.jsx
@@ -18,10 +18,9 @@ export default function CheeseInfo() {
   return (
     <div class="user-opinion">
       <img src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/72966651_2559285287452156_7846582019143237632_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=-c4xCAM_yeQAX_lt-_F&_nc_ht=scontent-lga3-2.xx&oh=d1b93c9ea2fc19c67e7cb885b00cfa24&oe=60E79F7A" alt="oopsies" />
-    <div class="right-side">
       <Navbar />
+      <Form class="user-form"setOpinions={setOpinions}/>
       <div class="user-interaction">
-      <Form setOpinions={setOpinions}/>
       {opinions &&
         opinions.map((opinion) => {
           return (
@@ -32,7 +31,6 @@ export default function CheeseInfo() {
           )
         })}
         </div>
-      </div>
       </div>
   )
 }
