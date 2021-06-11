04898f63ad93064943a49df6faf3b7a0c0b55222
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-11 12:58:55 -0400

    CSS user opinion page scroll bar working

--- src/components/UserOpinion.jsx
+++ src/components/UserOpinion.jsx
@@ -1,40 +1,44 @@
-import { useState, useEffect } from 'react'
-import { GetUserOpinion } from "../services/api"
-import Navbar from "./layout/Navbar"
-import Form from "./inputs/Form"
+import { useState, useEffect } from "react";
+import { GetUserOpinion } from "../services/api";
+import Navbar from "./layout/Navbar";
+import Form from "./inputs/Form";
 
 export default function CheeseInfo() {
-  const [opinions, setOpinions] = useState([])
+  const [opinions, setOpinions] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await GetUserOpinion()
-      setOpinions(res)
-    }
-    fetchData()
-  }, [])
-  
+      const res = await GetUserOpinion();
+      setOpinions(res);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="user-opinion">
-      <img src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/72966651_2559285287452156_7846582019143237632_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=-c4xCAM_yeQAX_lt-_F&_nc_ht=scontent-lga3-2.xx&oh=d1b93c9ea2fc19c67e7cb885b00cfa24&oe=60E79F7A" alt="oopsies" />
+    <div className="user-opinion-view">
+      <div className="user-bg-img"></div>
+      {/* <img
+        src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/72966651_2559285287452156_7846582019143237632_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=-c4xCAM_yeQAX_lt-_F&_nc_ht=scontent-lga3-2.xx&oh=d1b93c9ea2fc19c67e7cb885b00cfa24&oe=60E79F7A"
+        alt="oopsies"
+      /> */}
       <Navbar />
-      <Form className="user-form" setOpinions={setOpinions}/>
-      <div className="user-interaction">
-        <div></div>
-      {opinions &&
-        opinions.map((opinion) => {
-          return (
-            <div className="user-opinion-in-view">
-              <h2>{opinion.fields.name}</h2>
-              <hr className="divider"/>
-              <p>{opinion.fields.input}</p>
-            </div>
-          )
-        })}
-        <div></div>
-        </div>
+      <Form className="user-form" setOpinions={setOpinions} />
+      {/* <div className="user-opinion-cell"> */}
+      <div className="user-opinion-container">
+        {opinions &&
+          opinions.map((opinion) => {
+            return (
+              <div className="user-opinion-in-view">
+                <h2>{opinion.fields.name}</h2>
+                <hr className="divider" />
+                <p>{opinion.fields.input}</p>
+              </div>
+            );
+          })}
       </div>
-  )
+    </div>
+    // </div>
+  );
 }
 // style={{ marginBottom: '100px' }}
 //style={{height: '300px'}}
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
+    <div className="navbar opinion-navbar">
       <ul className="ul-links">
-
         <li className="li-links">
-          <Link to="/"
-            style={{ color: 'rgb(94, 73, 73)', textDecoration: 'none' }}>
-            <strong>|</strong>  Home  <strong>|</strong>  
-        </Link>
+          <Link
+            to="/"
+            style={{ color: "rgb(94, 73, 73)", textDecoration: "none" }}
+          >
+            <strong>|</strong> Home <strong>|</strong>
+          </Link>
         </li>
 
         <li className="li-links">
-          <Link to="/cheese-paring"
-            style={{ color: 'rgb(94, 73, 73)', textDecoration: 'none' }}>
-          <strong>|</strong>  Find Some Cheese Based Parings!  <strong>|</strong>  
-        </Link>
+          <Link
+            to="/cheese-paring"
+            style={{ color: "rgb(94, 73, 73)", textDecoration: "none" }}
+          >
+            <strong>|</strong> Find Some Cheese Based Parings!{" "}
+            <strong>|</strong>
+          </Link>
         </li>
 
         <li className="li-links">
-          <Link to="/user-opinion"
-            style={{ color: 'rgb(94, 73, 73)', textDecoration: 'none' }}>
-              <strong>|</strong>  User's Opinion  <strong>|</strong>  
-        </Link>
+          <Link
+            to="/user-opinion"
+            style={{ color: "rgb(94, 73, 73)", textDecoration: "none" }}
+          >
+            <strong>|</strong> User's Opinion <strong>|</strong>
+          </Link>
         </li>
       </ul>
     </div>
-  )
+  );
 }
